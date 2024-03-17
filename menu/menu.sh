e8c08cd4d4b21725b315d3af918afe3e661b2e93
yaudahhh <163600271+yaudahhh@users.noreply.github.com>
2024-03-18 02:52:04 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -409,7 +409,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
-echo -e " $COLOR1│${NC} ${COLBG1}                ${WH} • SFVT VPN TUNNELING •                 ${NC} $COLOR1│ $NC"
+echo -e " $COLOR1│${NC} ${COLBG1}                ${WH} • MINDFREAK VPN TUNNELING •                 ${NC} $COLOR1│ $NC"
 echo -e " $COLOR1╰══════════════════════════════════════════════════════════╯${NC}"
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ OS            ${COLOR1}: ${WH}$MODEL2${NC}"
